5ee76fffa1393ae77a71debb6452cf66441c48b4
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-08-01 16:43:02 +0100

    Adds basic header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,19 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      background: "#ebded5",
+      black: "#151817",
+      red: "#b61737",
+      pink: "#d19079",
+      pinkLigth: "#efaddb",
+      tumeric: "#ceb04b",
+      fountainBlue: "#54bec6",
+      green: "#90c2c2",
+    },
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
   },
   plugins: [],
 };
