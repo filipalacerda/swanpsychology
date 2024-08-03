1857c8ca190030b5df911aa9a89a6f0b7a15a240
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-08-03 16:37:08 +0100

    Adds services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,59 @@
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="md:p-24 p-12 flex md:mt-20 mt-10 gap-20 flex-col mb-10">
+      <div className="text-center">
+        <h2 className="text-5xl mx-14">Our Services</h2>
+      </div>
+      <div className="flex flex-col gap-y-10">
+        <div className="flex md:flex-row flex-col gap-10">
+          <div>
+            <h3 className="text-lg mb-4">Service 1</h3>
+            <p className="font-light">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <div>
+            <h3 className="text-lg mb-4">Service 2</h3>
+            <p className="font-light">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <div>
+            <h3 className="text-lg mb-4">Service 3</h3>
+            <p className="font-light">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+        <div className="flex md:flex-row flex-col gap-10">
+          <div>
+            <h3 className="text-lg mb-4">Service 4</h3>
+            <p className="font-light">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <div>
+            <h3 className="text-lg mb-4">Service 5</h3>
+            <p className="font-light">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <div>
+            <h3 className="text-lg mb-4">Service 6</h3>
+            <p className="font-light">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
